d3d01e104b9422256b79e581d5e6016e7f075c63
Teodor <teodorneishan@gmail.com>
2023-07-27 17:56:47 +0300

    viewer_protocol_policy = redirect-to-https

--- main.tf
+++ main.tf
@@ -259,7 +259,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
-
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "my-cloudfront"
@@ -269,7 +268,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
-
     forwarded_values {
       query_string = false
 
@@ -277,10 +275,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
+
   }
   restrictions {
     geo_restriction {
